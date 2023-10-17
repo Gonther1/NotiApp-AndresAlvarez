63ca0d4985c088d1e6cd0039214fb09490d97ae0
Gonther1 <126286883+Gonther1@users.noreply.github.com>
2023-10-17 13:31:45 -0500

    feat: :sparkles: Create all Repositories with the structure  basic
    @Gonther1

---
+++ Infrastructure/Repositories/TipoNotificacionRepository.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+
+namespace Infrastructure.Repositories;
+
+public class TipoNotificacionRepository : GenericRepository<TipoNotificacion>, ITipoNotificacionRepository
+{
+    private readonly NotiAppContext _context;
+
+    public TipoNotificacionRepository(NotiAppContext context) : base(context)
+    {
+        _context = context;
+    }
+}
