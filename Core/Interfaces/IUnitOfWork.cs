0137db05bc77c44168bbe5a02a843d1c62bac7e1
Gonther1 <luisandresalvarezsilva@gmail.com>
2023-10-17 07:49:39 -0500

    feat: :sparkles: Add all Interfaces to IUnitOfWork
    @Gonther1

--- Core/Interfaces/IUnitOfWork.cs
+++ Core/Interfaces/IUnitOfWork.cs
@@ -7,5 +7,20 @@ namespace Core.Interfaces;
 
 public interface IUnitOfWork
 {
+    IAuditoriaRepository Auditorias { get; }
+    IBlockChainRepository BlockChain { get; }
+    IEstadoNotificacionRepository EstadosNotificaciones { get; }
+    IFormatoRepository Formatos { get; }
+    IGenericoVsSubmoduloRepository GenericosVsSubmodulos { get; }
+    IHiloRespuestaRepository HilosRespuestas { get; }
+    IMaestroVsSubmodulosRepository Auditorias { get; }
+    IModuloMaestroRepository ModulosMaestros { get; }
+    IModuloNotificacionRepository ModulosNotificaciones { get; }
+    IRadicadoRepository Radicados { get; }
+    IRolRepository Roles { get; }
+    IRolvSMaestro RolesvSMaestros { get; }
+    ISubModuloRepository Submodulos { get; }
+    ITipoNotificacionRepository TiposNotificaciones { get; }
+    ITipoRequerimientoRepository TiposRequerimientos { get; }
     Task<int> SaveAsync();
 }
