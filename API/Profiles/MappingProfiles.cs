40394c55acedef44e0e60f587586aa91230ee826
Gonther1 <luisandresalvarezsilva@gmail.com>
2023-10-17 20:47:20 -0500

    feat: :sparkles: Add all Dtos in MappingProfiles
    @Gonther1

---
+++ API/Profiles/MappingProfiles.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+
+namespace API.Profiles;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Auditoria, AuditoriaDto>()
+        .ReverseMap();
+        
+        CreateMap<BlockChain, BlockChainDto>()
+        .ReverseMap();
+
+        CreateMap<EstadoNotificacion, EstadoNotificacionDto>()
+        .ReverseMap();
+
+        CreateMap<Formato, FormatoDto>()
+        .ReverseMap();
+
+        CreateMap<GenericoVsSubmodulo, GenericoVsSubmoduloDto>()
+        .ReverseMap();
+
+        CreateMap<HiloRespuestaNotificacion, HiloRespuestaNotificacionDto>()
+        .ReverseMap();
+
+        CreateMap<MaestroVsSubmodulos, MaestroVsSubmoduloDto>()
+        .ReverseMap();
+
+        CreateMap<ModuloMaestro, ModuloMaestroDto>()
+        .ReverseMap();
+
+        CreateMap<ModuloNotificacion, ModuloNotificacionDto>()
+        .ReverseMap();
+        
+    }
+}
